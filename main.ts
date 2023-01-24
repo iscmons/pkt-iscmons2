b5cdbe142664228367af22e5c5dae9d1a7fd287e
iscmons <123409610+iscmons@users.noreply.github.com>
2023-01-24 13:29:37 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,20 @@
 //% weight=100 color=#70c0f0 icon="\uf042" block="ISCMONS"
 namespace iscmons {
 	
+    // TMP36 variables
 	let TMP36_voltage = 0;
 	let TMP36_temp = 0;
 	let a = 0;
+
+    // BMP180 variables
+    let BMP180_adr = 0;
+    let BMP180_data_pressure;
+    let BMP180_data_temperature;
 		
 	/**
-    * read temperature
+    * TMP36 : read temperature
     */
-    //% blockId="ISCMONS_TMP36" block="TMP36 read in C"
+    //% blockId="ISCMONS_TMP36" block="TMP36 read T in C"
     //% weight=80 blockGap=8
     //% parts=tmp36 trackArgs=0
 	export function TMP36_read_celsius(): void {
@@ -23,4 +29,41 @@ namespace iscmons {
         control.waitMicros(4000);
     	//return a;
 	}
+
+    /**
+    * BMP180 : read temperature
+    */
+
+    //% blockId="ISCMONS_BMP180" block="definir BMP"
+    //% weight=80 blockGap=8
+    //% parts=tmp36 trackArgs=0
+    export function BMP180_create(adr_i2c: number): void {
+        BMP180_adr = adr_i2c;
+    }
+
+    function getreg(reg: number): number {
+        pins.i2cWriteNumber(BMP180_adr, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(BMP180_adr, NumberFormat.UInt8BE);
+    }
+
+    //% blockId="ISCMONS_BMP180" block="BMP read T in C"
+    //% weight=80 blockGap=8
+    //% parts=tmp36 trackArgs=0
+    export function BMP180_read_temperature(): number {
+        BMP180_data_temperature = (getreg(0xFA) << 12) + (getreg(0xFB) << 4) + (getreg(0xFC) >> 4)
+        BMP180_data_temperature = ((BMP180_data_temperature * 5 + 128) >> 8) / 100.
+        return BMP180_data_temperature;
+    }
+
+    //% blockId="ISCMONS_BMP180" block="BMP read p in Pa"
+    //% weight=80 blockGap=8
+    //% parts=tmp36 trackArgs=0
+    export function BMP180_read_pressure(): number {
+        BMP180_data_pressure = (getreg(0xF7) << 12) + (getreg(0xF8) << 4) + (getreg(0xF9) >> 4)
+        return BMP180_data_pressure;
+    }
+
+    
+
+
 }

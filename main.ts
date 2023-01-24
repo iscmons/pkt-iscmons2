ae03406d9dd588256fc861067559e6040649e634
iscmons <123409610+iscmons@users.noreply.github.com>
2023-01-24 14:04:14 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,26 +3,16 @@
  */
 //% weight=100 color=#70c0f0 icon="\uf042" block="ISCMONS"
 namespace iscmons {
-	
-    // TMP36 variables
-	let TMP36_voltage = 0;
-	let TMP36_temp = 0;
-	let a = 0;
-
-    // BMP180 variables
-    let BMP180_adr = 0;
-    let BMP180_data_pressure;
-    let BMP180_data_temperature;
 		
 	/**
-    * TMP36 : read temperature
+    * TMP36
     */
     //% blockId="ISCMONS_TMP36" block="TMP36 read T in C"
     //% weight=80 blockGap=8
-    //% parts=tmp36 trackArgs=0
+    //% parts=TMP36 trackArgs=0
 	export function TMP36_read_celsius(): void {
-		TMP36_voltage = pins.P0.analogRead();
-		TMP36_temp = 50*TMP36_voltage + 20;
+		let TMP36_voltage = pins.P0.analogRead();
+		let TMP36_temp = 50*TMP36_voltage + 20;
 		pins.LED.digitalWrite(true);
     	control.waitMicros(4000);
         pins.LED.digitalWrite(false);
@@ -31,37 +21,48 @@ namespace iscmons {
 	}
 
     /**
-    * BMP180 : read temperature
+    * BMP180
     */
-
-    //% blockId="ISCMONS_BMP180" block="definir BMP"
-    //% weight=80 blockGap=8
-    //% parts=tmp36 trackArgs=0
-    export function BMP180_create(adr_i2c: number): void {
-        BMP180_adr = adr_i2c;
+    function getreg(adr: number, reg: number): number {
+        pins.i2cWriteNumber(adr, reg, NumberFormat.UInt8BE);
+        return pins.i2cReadNumber(adr, NumberFormat.UInt8BE);
     }
 
-    function getreg(reg: number): number {
-        pins.i2cWriteNumber(BMP180_adr, reg, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(BMP180_adr, NumberFormat.UInt8BE);
-    }
-
-    //% blockId="ISCMONS_BMP180" block="BMP read T in C"
+    //% blockId="ISCMONS_BMP180" block="BMP reads T in C"
     //% weight=80 blockGap=8
-    //% parts=tmp36 trackArgs=0
-    export function BMP180_read_temperature(): number {
-        BMP180_data_temperature = (getreg(0xFA) << 12) + (getreg(0xFB) << 4) + (getreg(0xFC) >> 4)
+    //% parts=BMP180 trackArgs=0
+    export function BMP180_read_temperature(adr_i2c: number): number {
+        let BMP180_data_temperature = (getreg(adr_i2c, 0xFA) << 12) + (getreg(adr_i2c, 0xFB) << 4) + (getreg(adr_i2c,0xFC) >> 4)
         BMP180_data_temperature = ((BMP180_data_temperature * 5 + 128) >> 8) / 100.
         return BMP180_data_temperature;
     }
 
-    //% blockId="ISCMONS_BMP180" block="BMP read p in Pa"
+    //% blockId="ISCMONS_BMP180" block="BMP reads p in Pa"
     //% weight=80 blockGap=8
-    //% parts=tmp36 trackArgs=0
-    export function BMP180_read_pressure(): number {
-        BMP180_data_pressure = (getreg(0xF7) << 12) + (getreg(0xF8) << 4) + (getreg(0xF9) >> 4)
+    //% parts=BMP180 trackArgs=0
+    export function BMP180_read_pressure(adr_i2c: number): number {
+        let BMP180_data_pressure = (getreg(adr_i2c, 0xF7) << 12) + (getreg(adr_i2c, 0xF8) << 4) + (getreg(adr_i2c,0xF9) >> 4)
         return BMP180_data_pressure;
     }
+
+    /**
+    * HCSR04
+    */
+
+    //% blockId="ISCMONS_BMP180" block="HCSR04 reads distance in mm"
+    //% weight=80 blockGap=8
+    //% parts=HCSR04 trackArgs=0
+    export function HCSR04(pin: number): number {
+        pins.P0.digitalWrite(false);
+        control.waitMicros(5);
+        pins.P0.digitalWrite(true);
+        control.waitMicros(10);
+        pins.P0.digitalWrite(false);
+        control.waitForEvent(0, 0);
+        let data = pins.P1.pulseIn(PulseValue.High);
+        return data*100//582;
+    }
+    
 
     
 

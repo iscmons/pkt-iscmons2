d339126efc6e823076baa93fe5e566ddb2d133a2
iscmons <123409610+iscmons@users.noreply.github.com>
2023-01-24 08:58:43 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
-
-let TMP36pin = 0;
-    
-function TMP36read (text: string) {
-    TMP36pin = pins.P0.analogRead();
-    return TMP36pin;
+function doSomething () {
+    console.log("")
 }
-
+forever(function () {
+	
+})

bde007455418eb351aef5474090e4fccac0090b0
iscmons <123409610+iscmons@users.noreply.github.com>
2023-01-24 10:24:14 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
-function doSomething () {
-    console.log("")
-}
-forever(function () {
+/**
+ * ISCMONS block
+ */
+//% weight=100 color=#70c0f0 icon="\uf042" block="ISCMONS"
+namespace iscmons {
 	
-})
+	let a = 0;
+	
+	function get(): void{
+		a = 1;
+	}
+	
+	/**
+     	* get pressure
+     	*/
+    	//% blockId="ISCMONS_TMP36" block="tmp36"
+    	//% weight=80 blockGap=8
+    	//% parts=tmp36 trackArgs=0
+	export function doSomething(): number {
+		get();
+    		return a;
+	}
+}

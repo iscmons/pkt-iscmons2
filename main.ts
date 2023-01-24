7a7a6cfe40a18c812d2547a6c8cd8a5ad0a4fc65
iscmons <123409610+iscmons@users.noreply.github.com>
2023-01-24 08:56:14 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-forever(function () {
-	
-})
+
+/**
+ * ISCMONS block
+ */
+//% weight=100 color=#70c0f0 icon="\uf042" block="iscmons"
+namespace bme280 {
+    let TMP36pin = 0;
+    
+    /**
+     * get humidity
+     */
+    //% blockId="BME280_HUMIDITY" block="humidity"
+    //% weight=80 blockGap=8
+    //% parts=bme280 trackArgs=0
+    function TMP36read (text: string) {
+        TMP36pin = pins.P0.analogRead();
+        return TMP36pin;
+    }
+}

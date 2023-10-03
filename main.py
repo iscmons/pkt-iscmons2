fe70d380b8d4bc7afd0c729df96ee86e7a23c99a
iscmons <123409610+iscmons@users.noreply.github.com>
2023-10-03 13:58:27 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ ISCMONS block
 
 """
 # % weight=100 color=#70c0f0 icon="\uf042" block="ISCMONS"
-"""
-
-Servo moteur
-
-"""
-# % blockId="ISCMONS_SERVO" block="Motor turn in "
-# % weight=80 blockGap=8
-# % parts=HCSR04 trackArgs=0
 @namespace
 class iscmons:
     """
@@ -19,12 +11,13 @@ class iscmons:
     TMP36
     
     """
-    # % blockId="ISCMONS_TMP36" block="TMP36 read T in C"
+    # % blockId="ISCMONS_TMP36" block="reads T in C on %pin with TMP36"
     # % weight=80 blockGap=8
     # % parts=TMP36 trackArgs=0
+    # % group="TMP36"  
     # return a;
-    def TMP36_read_celsius():
-        TMP36_voltage = pins.P0.analog_read()
+    def TMP36_read_celsius(pin: AnalogInPin):
+        TMP36_voltage = pin.analog_read()
         TMP36_temp = 50 * TMP36_voltage + 20
         print("coucou \n")
         # pins.LED.digitalWrite(true);
@@ -40,33 +33,98 @@ class iscmons:
     def getreg(adr: number, reg: number):
         pins.i2c_write_number(adr, reg, NumberFormat.UINT8_BE)
         return pins.i2c_read_number(adr, NumberFormat.UINT8_BE)
-    # % blockId="ISCMONS_BMP180T" block="BMP reads T in C"
+    # % blockId="ISCMONS_BMP180T" block="reads T in C with BMP180 on I2C %id"
     # % weight=80 blockGap=8
     # % parts=BMP180 trackArgs=0
-    def BMP180_read_temperature(adr_i2c: number):
-        BMP180_data_temperature = (getreg(adr_i2c, 0xFA) << 12) + (getreg(adr_i2c, 0xFB) << 4) + (getreg(adr_i2c, 0xFC) >> 4)
+    # % group="BMP180"  
+    def BMP180_read_temperature(id2: number):
+        BMP180_data_temperature = (getreg(id2, 0xFA) << 12) + (getreg(id2, 0xFB) << 4) + (getreg(id2, 0xFC) >> 4)
         BMP180_data_temperature = ((BMP180_data_temperature * 5 + 128) >> 8) / 100.
         return BMP180_data_temperature
-    # % blockId="ISCMONS_BMP180P" block="BMP reads p in Pa"
+    # % blockId="ISCMONS_BMP180P" block="reads p in Pa with BMP180 on I2C %id"
     # % weight=80 blockGap=8
     # % parts=BMP180 trackArgs=0
-    def BMP180_read_pressure(adr_i2c2: number):
-        BMP180_data_pressure = (getreg(adr_i2c2, 0xF7) << 12) + (getreg(adr_i2c2, 0xF8) << 4) + (getreg(adr_i2c2, 0xF9) >> 4)
+    # % group="BMP180"  
+    def BMP180_read_pressure(id3: number):
+        BMP180_data_pressure = (getreg(id3, 0xF7) << 12) + (getreg(id3, 0xF8) << 4) + (getreg(id3, 0xF9) >> 4)
         return BMP180_data_pressure
     """
     
     HCSR04
     
     """
-    # % blockId="ISCMONS_HCSR04" block="HCSR04 reads distance in cm"
+    # % blockId="ISCMONS_HCSR04" block="reads distance in cm with HCSR04 with echo on %echo with trigger on %trig"
     # % weight=80 blockGap=8
     # % parts=HCSR04 trackArgs=0
-    def HCSR04_measure():
-        pins.P0.digital_write(False)
+    # % group="HCSR04"  
+    def HCSR04_measure(echo: DigitalInOutPin, trig: DigitalInOutPin):
+        trig.digital_write(False)
         control.wait_micros(5)
-        pins.P0.digital_write(True)
+        trig.digital_write(True)
         control.wait_micros(10)
-        pins.P0.digital_write(False)
+        trig.digital_write(False)
         control.wait_for_event(0, 0)
-        data = pins.P1.pulse_in(PulseValue.HIGH)
+        data = echo.pulse_in(PulseValue.HIGH)
         return (data * 2) / 29.1
+    """
+    
+    BUZZER (checked)
+    
+    """
+    # % blockId="ISCMONS_BUZZER" block="turns %bool buzzer on %pin"
+    # % weight=80 blockGap=8
+    # % parts=BUZZER trackArgs=0
+    # % group="BUZZER"  
+    def Buzzer(pin2: DigitalInOutPin, bool2: bool):
+        # pins.P1.digitalWrite(bool);
+        pin2.digital_write(bool2)
+        return
+    """
+    
+    SH1106 screen
+    
+    """
+    # % blockId="ISCMONS_SH1106" block="SH1106 displays"
+    # % weight=80 blockGap=8
+    # % parts=SH1106 trackArgs=0
+    # % group="Screen"  
+    def SH1106_display():
+        return
+    # % blockId="ISCMONS_SH1106" block="SH1106 changes contrast"
+    # % weight=80 blockGap=8
+    # % parts=SH1106 trackArgs=0
+    # % group="Screen"  
+    def SH1106_setcontrast():
+        return
+    # % blockId="ISCMONS_SH1106" block="SH1106 sets text"
+    # % weight=80 blockGap=8
+    # % parts=SH1106 trackArgs=0
+    # % group="Screen"  
+    def SH1106_settext():
+        return
+    """
+    
+    Data logger
+    
+    """
+    # % blockId="ISCMONS_File" block="writes %text in %filename"
+    # % weight=80 blockGap=8
+    # % parts=SH1106 trackArgs=0
+    # % group="Data logger"  
+    def write_filename(filename: str, text: str):
+        storage.append_line(filename, text)
+    """
+    
+    Servo moteur
+    
+    """
+    # % blockId="ISCMONS_Servo" block="turns on the servo on %pin with %rate percent during %microsecond"
+    # % weight=80 blockGap=8
+    # % parts=SH1106 trackArgs=0
+    # % group="Servo moteur"  
+    def servo_turn(pin3: DigitalInOutPin, rate: number, millisecond: number):
+        for i in range(millisecond * 10):
+            pin3.digital_write(True)
+            control.wait_micros(rate)
+            pin3.digital_write(False)
+            control.wait_micros(100 - rate)
